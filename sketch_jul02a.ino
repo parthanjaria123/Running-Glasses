31c1a4587ded9e5224a95e04c30ded1052dbe2d5
parthanjaria123 <45218044+parthanjaria123@users.noreply.github.com>
2019-07-08 11:42:09 -0700

    Draft 1

--- sketch_jul02a.ino
+++ sketch_jul02a.ino
@@ -1,74 +1,108 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 SoftwareSerial ss(3,4);
 TinyGPSPlus gps;
+Adafruit_SSD1306 display(128,32,&Wire,4);
+
+float distance=0;
+float altIni;
 
 void setup() {
-ss.begin(9600);
-Serial.begin(115200);
-
-float lat1 = latitude();
-float lng1 = longitude();
-
-Serial.println(lat1,6);
-Serial.println(lng1,6);
-
-delay(10000);
-
-float lat2 = latitude();
-float lng2 = longitude();
-
-Serial.println(lat2,6);
-Serial.println(lng2,6);
-
-Serial.println("Dist: ");
-Serial.println(gps.distanceBetween(lat1,lng1,lat2,lng2));
-
+  ss.begin(9600);
+  Serial.begin(115200);
+  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("Allocation failed"));
+    for(;;);
+  }
+  
+  display.clearDisplay();
+  altIni = alt();
 }
 
 void loop() {
-/*if(gps.location.lat()==0){
+  float lat1 = latitude();
+  float lng1 = longitude();
+
+  delay(10000);
+
+  float lat2 = latitude();
+  float lng2 = longitude();
+  
+  float changeInDistance = gps.distanceBetween(lat1,lng1,lat2,lng2);
+  distance += changeInDistance;
+  float distanceInKM = distance/1000;
+
+  display.clearDisplay();  
+
+  display.setCursor(0,0);
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  
+  display.print("P A C E: ");
+
+  float runningPace = (1/gps.speed.mph())*60;
+
+  if(runningPace>30){
+    display.print(" >30");
+  }
+  else if(runningPace>10){
+    display.print((int) runningPace);
+  }
+  else{
+    display.print(runningPace,2);
+  }
+    
+  display.setCursor(0,10);
+  display.print("M L G E: ");
+  display.print(distanceInKM,2);
+
+  float altnew = alt();
+  float changeInAlt = altnew - altIni;
+  
+  display.setCursor(0,21);
+  display.print(" A L T: ");
+  display.print(changeInAlt,2);
+  display.display();
+  
 }
-else{
-Serial.print("Latitude: ");
-Serial.println(gps.location.lat());}
-if(gps.location.lng()==0){
-}
-else{
-Serial.print("Longitude: ");
-Serial.println(gps.location.lng());}
-if(ss.available()>0){
-if(gps.encode(ss.read())){
-if(gps.location.isValid()){
-  Serial.print("Latitude: ");
-  Serial.println(gps.location.lat());
-  Serial.print("Longitude: ");
-  Serial.println(gps.location.lng());
-}
-}
-}*/
-}
+
 float latitude(){
-while(true){
-if(ss.available()>0){
-if(gps.encode(ss.read())){
-if(gps.location.isUpdated()){
-  return (gps.location.lat());
-}
-}
-}
-}
+  while(true){
+    if(ss.available()>0){
+      if(gps.encode(ss.read())){
+        if(gps.location.isUpdated()){
+          return (gps.location.lat());
+        }
+      }
+    }
+  }
 }
 
 float longitude(){
-while(true){
-if(ss.available()>0){
-if(gps.encode(ss.read())){
-if(gps.location.isUpdated()){
-  return (gps.location.lng());
+  while(true){
+    if(ss.available()>0){
+      if(gps.encode(ss.read())){
+        if(gps.location.isUpdated()){
+          return (gps.location.lng());
+        }
+      }
+    }
+  }
 }
-}
-}
-}
+
+float alt(){
+  while(true){
+    if(ss.available()>0){
+      if(gps.encode(ss.read())){
+        if(gps.altitude.isUpdated()){
+          return (gps.altitude.meters());
+        }
+      }
+    }
+  }
 }
